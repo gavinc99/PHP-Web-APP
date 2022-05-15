aebc3d33ececb90832ec130831486a98f20568d7
Gavin Corr <60385642+gavinc99@users.noreply.github.com>
2022-05-15 09:11:43 +0100

    Delete Sit2Fit directory

--- Sit2Fit/footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-  <div s1>
-    <h3>Follow us on Facebook and Twitter!
-      <a href="https://www.facebook.com"><img src="images/fb.png" id="fb" alt="Facebook"></a>
-      <a href="https://twitter.com"><img src="images/tw.png" id="tw" alt="Twitter"></a></h3>
-  </div>
-</footer>
